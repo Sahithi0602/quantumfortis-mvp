78331b5fa4ecc8aeaacf88765189535601e4ceb0
ikalavak <kvmahesh2003@gmail.com>
2025-05-26 15:53:49 +0000

    feat: deployed working lambda with KMS integration

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,15 +1,31 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as ddb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as path from 'path';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // ✅ DynamoDB table: KeyInventory
-    const inventory = new ddb.Table(this, 'KeyInventory', {
-      partitionKey: { name: 'keyId', type: ddb.AttributeType.STRING },
-      billingMode: ddb.BillingMode.PAY_PER_REQUEST,
+    // Lambda function using local index.js
+    const listKeys = new lambda.Function(this, 'ListKeysFn', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../../lambda')),
+      timeout: cdk.Duration.seconds(15),
+    });
+
+    // ✅ Add permission to call KMS
+    listKeys.addToRolePolicy(new iam.PolicyStatement({
+      actions: ['kms:ListKeys'],
+      resources: ['*'],
+    }));
+
+    // API Gateway exposing the Lambda
+    new apigw.LambdaRestApi(this, 'InventoryApi', {
+      handler: listKeys,
     });
   }
 }
