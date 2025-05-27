9c46f29fef24e38afbdd01b886f18a990a5e631e
Sahithi0602 <sahithi.feb2000@gmail.com>
2025-05-27 18:36:11 +0000

    chore: finalize QuantumFortisInventoryStack with tagging

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,11 +1,15 @@
+import * as cdk from 'aws-cdk-lib';
 import { Tags } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 
 export class QuantumFortisInventoryStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // ✅ Tag all resources with the project name
+    // ✅ Add default AWS resource tag
     Tags.of(this).add('Project', 'QuantumFortis');
+
+    // 🧱 Add resources here in the future
   }
 }
 
